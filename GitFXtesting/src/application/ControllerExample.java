90b479222e5186bd3201dcf817f3329b2682338b
gennpan <panicogennaro99@gmail.com>
2022-03-11 11:27:00 +0100

    Second Functional Commit Test (ControllerExample added)

---
+++ GitFXtesting/src/application/ControllerExample.java
@@ -0,0 +1,11 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class ControllerExample {
+
+	public void up(ActionEvent e) {
+		System.out.println("Click on Up button!");
+	}
+
+}
